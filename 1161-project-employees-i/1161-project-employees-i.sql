90e962f7e3fe91919ee5711ddef41afc6455fb56
Doaa Tawfik <126175479+DoaaTawfik0@users.noreply.github.com>
2024-11-26 16:15:24 +0200

    Time: 1809 ms (13.2%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -1,15 +1,17 @@
 /* Write your T-SQL query statement below */
 
 
-WITH T1 AS
-(
-    SELECT p.project_id,COUNT(e.employee_id) employeeCount,sum(e.experience_years) yearsCount
-    FROM Employee e JOIN Project p
-    ON e.employee_id = p.employee_id
-    GROUP BY p.project_id
-)
-SELECT project_id, 
-       ROUND(CAST(yearsCount * 100 AS FLOAT) / employeeCount, 0) / 100 AS average_years
-FROM T1
-ORDER BY project_id;
+
+
+SELECT p.project_id,
+       ROUND(AVG(CAST(experience_years AS DECIMAL(10,2))), 2) AS average_years
+FROM Employee e JOIN Project p
+ON e.employee_id = p.employee_id
+GROUP BY p.project_id
+ORDER BY P.project_id
+
+
+
+
+
 
